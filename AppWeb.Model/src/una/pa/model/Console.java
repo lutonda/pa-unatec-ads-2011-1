ef5df0ad1584d5f5fa99eaeabe0f2a109dacfa3c
ciqueira <ciqueira@gmail.com>
2011-04-20 01:56:35 +0000

    Model

---
+++ AppWeb.Model/src/una/pa/model/Console.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package una.pa.model;
+
+/**
+ *
+ * @author Magno
+ */
+public class Console {
+    protected int id_console;
+    protected String ds_console;
+
+    public String getDs_console() {
+        return ds_console;
+    }
+
+    public void setDs_console(String ds_console) {
+        this.ds_console = ds_console;
+    }
+
+    public int getId_console() {
+        return id_console;
+    }
+
+    public void setId_console(int id_console) {
+        this.id_console = id_console;
+    }
+}

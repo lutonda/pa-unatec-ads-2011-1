2c5ada364cdd6fe8e9670d270466fb789729c45a
tiagokiokushin@gmail.com <tiagokiokushin@gmail.com@7d76fdf4-5015-b27e-b386-42a4ce47dd1f>
2011-04-28 03:24:31 +0000

    Atualizaçao da Tebela Desenvolvedor, Editora, com as classes ja feitas de listar, incluir, excluir e editar

--- trunk/AppWeb.Model/src/una/pa/model/Console.java
+++ trunk/AppWeb.Model/src/una/pa/model/Console.java
@@ -1,14 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package una.pa.model;
 
-/**
- *
- * @author Magno 
- */
 public class Console {
     protected int id_console;
     protected String ds_console;
---
+++ trunk/AppWeb.Model/src/una/pa/model/Desenvolvedor.java
@@ -0,0 +1,23 @@
+package una.pa.model;
+
+public class Desenvolvedor {
+    protected int id_desenv;
+    protected String ds_desenv ;
+
+    public String getDs_desenv() {
+        return ds_desenv;
+    }
+
+    public void setDs_desenv(String ds_desenv) {
+        this.ds_desenv = ds_desenv;
+    }
+
+    public int getId_desenv() {
+        return id_desenv;
+    }
+
+    public void setId_desenv(int id_desenv) {
+        this.id_desenv = id_desenv;
+    }
+
+}

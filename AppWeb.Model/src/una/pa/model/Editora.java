0a462e669c00b547c87341b97c120acbd87224aa
tiagokiokushin <tiagokiokushin@gmail.com>
2011-04-28 03:24:31 +0000

    Atualizaçao da Tebela Desenvolvedor, Editora, com as classes ja feitas de listar, incluir, excluir e editar

---
+++ AppWeb.Model/src/una/pa/model/Editora.java
@@ -0,0 +1,23 @@
+package una.pa.model;
+
+public class Editora {
+    protected int id_editora;
+    protected String nm_editora;
+
+    public int getId_editora() {
+        return id_editora;
+    }
+
+    public void setId_editora(int id_editora) {
+        this.id_editora = id_editora;
+    }
+
+    public String getNm_editora() {
+        return nm_editora;
+    }
+
+    public void setNm_editora(String nm_editora) {
+        this.nm_editora = nm_editora;
+    }
+
+}
---
+++ AppWeb.Model/src/una/pa/model/Genero.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package una.pa.model;
+
+/**
+ *
+ * @author Tiago
+ */
+public class Genero {
+    protected int id_genero;
+    protected String ds_genero;
+
+    public String getDs_genero() {
+        return ds_genero;
+    }
+
+    public void setDs_genero(String ds_genero) {
+        this.ds_genero = ds_genero;
+    }
+
+    public int getId_genero() {
+        return id_genero;
+    }
+
+    public void setId_genero(int id_genero) {
+        this.id_genero = id_genero;
+    }
+
+
+}

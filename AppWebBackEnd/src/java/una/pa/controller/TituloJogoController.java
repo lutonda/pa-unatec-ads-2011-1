d01fe71153fc9e5d81ab1d6645ff4953c5cf87f4
ciqueira <ciqueira@7d76fdf4-5015-b27e-b386-42a4ce47dd1f>
2011-05-01 23:36:29 +0000

    inclusão edição e exclusão itens titulo jogos

---
+++ AppWebBackEnd/src/java/una/pa/controller/TituloJogoController.java
@@ -0,0 +1,140 @@
+package una.pa.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
+import una.pa.model.*;
+import una.pa.service.*;
+
+import java.util.*;
+
+public class TituloJogoController extends MultiActionController {
+
+    public ModelAndView editar(HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+
+        TituloJogo objC = TituloJogoService.unico(Integer.parseInt(request.getParameter("id")));
+
+        List<Editora> objEditora = EditoraService.listar();
+        List<Genero> objGnero = GeneroService.listar();
+        List<Desenvolvedor> objDesenv = DesenvolvedorService.listar();
+
+        ModelAndView mav = new ModelAndView("titulojogo/editar");
+        mav.addObject("titulojogo", objC);
+        mav.addObject("editoras", objEditora);
+        mav.addObject("generos", objGnero);
+        mav.addObject("desenvolvedores", objDesenv);
+        return mav;
+        //return new ModelAndView("console/editar", "console", objC);
+    }
+
+    public ModelAndView editarForm(HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+        boolean flag = false;
+        TituloJogo objC = new TituloJogo();
+        objC.setId_titulo_jogo(Integer.parseInt(request.getParameter("id_titulo_jogo")));
+        objC.setId_editora(Integer.parseInt(request.getParameter("id_editora")));
+        objC.setId_genero(Integer.parseInt(request.getParameter("id_genero")));
+        objC.setId_desenv(Integer.parseInt(request.getParameter("id_desenv")));
+        objC.setNm_titulo(request.getParameter("nm_titulo"));
+        objC.setTipo(request.getParameter("tipo"));
+
+        ModelAndView mav = new ModelAndView("titulojogo/editar");
+        try {
+
+            flag = TituloJogoService.alterar(objC);
+
+        } catch (Exception e) {
+            mav.addObject("msg", e.getMessage());
+        }
+
+        if (flag)
+            response.sendRedirect("listar.htm");
+
+        List<Editora> objEditora = EditoraService.listar();
+        List<Genero> objGnero = GeneroService.listar();
+        List<Desenvolvedor> objDesenv = DesenvolvedorService.listar();
+        
+        mav.addObject("titulojogo", objC);
+        mav.addObject("editoras", objEditora);
+        mav.addObject("generos", objGnero);
+        mav.addObject("desenvolvedores", objDesenv);
+       
+        return mav;
+        //return new ModelAndView("titulojogo/editar", "titulojogo", objC);
+    }
+
+    public ModelAndView incluir(HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+        ModelAndView mav = new ModelAndView("titulojogo/incluir");
+
+        List<Editora> objEditora = EditoraService.listar();
+        List<Genero> objGnero = GeneroService.listar();
+        List<Desenvolvedor> objDesenv = DesenvolvedorService.listar();
+
+        mav.addObject("editoras", objEditora);
+        mav.addObject("generos", objGnero);
+        mav.addObject("desenvolvedores", objDesenv);
+
+        return mav;
+        //return new ModelAndView("titulojogo/incluir", "msg", "");
+    }
+
+    public ModelAndView incluirForm(HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+        boolean flag;
+        TituloJogo objC = new TituloJogo();
+        objC.setId_editora(Integer.parseInt(request.getParameter("id_editora")));
+        objC.setId_genero(Integer.parseInt(request.getParameter("id_genero")));
+        objC.setId_desenv(Integer.parseInt(request.getParameter("id_desenv")));
+        objC.setNm_titulo(request.getParameter("nm_titulo"));
+        objC.setTipo(request.getParameter("tipo"));
+
+        try {
+
+            flag = TituloJogoService.incluir(objC);
+
+        } catch (Exception e) {
+            return new ModelAndView("titulojogo/incluir", "msg", e.getMessage());
+        }
+
+        if (flag) {
+            response.sendRedirect("listar.htm");
+        }
+
+        return new ModelAndView("titulojogo/incluir", "titulojogo", objC);
+    }
+
+    public ModelAndView remove(HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+        boolean flag;
+        try {
+
+            flag = TituloJogoService.excluir(Integer.parseInt(request.getParameter("id")));
+
+        } catch (Exception e) {
+            return new ModelAndView("titulojogo/listagem", "msg", e.getMessage());
+        }
+
+        if (flag) {
+            response.sendRedirect("listar.htm");
+        }
+
+        return new ModelAndView("titulojogo/listagem", "msg", "Erro ao excluir.");
+    }
+
+    public ModelAndView listar(HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+        List<TituloJogo> objC;
+        try {
+
+            objC = TituloJogoService.listar();
+
+        } catch (Exception e) {
+            return new ModelAndView("titulojogo/listagem", "msg", e.getMessage());
+        }
+        return new ModelAndView("titulojogo/listagem", "titulo_jogos", objC);
+    }
+}
---
+++ AppWebBackEnd/web/WEB-INF/jsp/titulojogo/inc_formulario.jsp
@@ -0,0 +1,96 @@
+
+<div id="content" class="clearfix">
+    <p>
+        <a href="listar.htm" title="Voltar para a listagem">&lt; Voltar para a listagem</a>
+    </p>
+    <h2>Incluir Console</h2>
+
+    <div class="frm_content">
+
+        <ul class="frm clearfix">
+            <li>
+                <input type="submit" value="Salvar" class="bt" />
+            </li>
+            <li>
+                ${msg}
+            </li>
+            <li>
+                <label><strong>Nome Titulo</strong></label>
+                <input type="text" size="80" name="nm_titulo" value="${titulojogo.nm_titulo}"/>
+            </li>
+            <li>
+                <label><strong>Tipo</strong></label>
+                <select name="tipo">
+                    <option value="0">Selecione...</option>
+                    <c:if test="${(titulojogo.tipo == 'Single Player')&&(titulojogo.tipo != 'Multi Player')}">
+                        <option selected="selected">Single Player</option>
+                        <option>Multi Player</option>
+                    </c:if>
+                    <c:if test="${(titulojogo.tipo == 'Multi Player')&&(titulojogo.tipo != 'Single Player')}">
+                        <option>Single Player</option>
+                        <option selected="selected">Multi Player</option>
+                    </c:if>
+                    <c:if test="${(titulojogo.tipo != 'Multi Player')&&(titulojogo.tipo != 'Single Player')}">
+                        <option value="Single Player">Single Player</option>
+                        <option value="Multi Player">Multi Player</option>
+                    </c:if>
+                </select>
+            </li>
+            <li>
+                <label><strong>Editora</strong></label>
+                <select name="id_editora">
+                    <option value="0">Selecione...</option>
+                    <c:forEach items="${editoras}" var="Editora" >
+                        <c:if test="${Editora.id_editora == titulojogo.id_editora}">
+                            <c:set var="select" value="selected=\"selected\""/>
+                        </c:if>
+                        <c:if test="${Editora.id_editora != titulojogo.id_editora}">
+                            <c:set var="select" value=""/>
+                        </c:if>
+                        <option ${select} value="${Editora.id_editora}">${Editora.nm_editora}</option>
+                    </c:forEach>
+                </select>
+            </li>
+            <li>
+                <label><strong>Genero</strong></label>
+                <select name="id_genero">
+                    <option value="0">Selecione...</option>
+                    <c:forEach items="${generos}" var="Genero" >
+                        <c:if test="${Genero.id_genero == titulojogo.id_genero}">
+                            <c:set var="select" value="selected=\"selected\""/>
+                        </c:if>
+                        <c:if test="${Genero.id_genero != titulojogo.id_genero}">
+                            <c:set var="select" value=""/>
+                        </c:if>
+                        <option ${select} value="${Genero.id_genero}">${Genero.ds_genero}</option>
+                    </c:forEach>
+                </select>
+            </li>
+            <li>
+                <label><strong>Desenvolvedor</strong></label>
+                <select name="id_desenv">
+                    <option value="0">Selecione...</option>
+                    <c:forEach items="${desenvolvedores}" var="Desenvolvedor" >
+                        <c:if test="${Desenvolvedor.id_desenv == titulojogo.id_desenv}">
+                            <c:set var="select" value="selected=\"selected\""/>
+                        </c:if>
+                        <c:if test="${Desenvolvedor.id_desenv != titulojogo.id_desenv}">
+                            <c:set var="select" value=""/>
+                        </c:if>
+                        <option ${select} value="${Desenvolvedor.id_desenv}">${Desenvolvedor.ds_desenv}</option>
+                    </c:forEach>
+                </select>
+            </li>
+
+            <li>
+                <input type="submit" value="Salvar" class="bt" />
+            </li>
+        </ul>
+    </div>
+
+    <p>
+        <a href="listar.htm" title="Voltar para a listagem">&lt; Voltar para a listagem</a>
+    </p>
+</div>
+
+

7fca8102ec0923816478665f29703a4f5af32b19
tiagokiokushin <tiagokiokushin@gmail.com>
2011-05-02 01:33:50 +0000

    Atualizando a Editora

--- AppWeb.Service/src/una/pa/service/EditoraService.java
+++ AppWeb.Service/src/una/pa/service/EditoraService.java
@@ -9,6 +9,19 @@ public class EditoraService {
      public static List<Editora> listar() {
         return EditoraDal.listarEditora();
     }
+      public static Editora unico(int id) {
+
+        List<Editora> lObjec = EditoraDal.listarEditora();
+
+        Editora retorno = new Editora();
+        for (Iterator<Editora> it = lObjec.iterator(); it.hasNext();) {
+            Editora editora = it.next();
+
+            if (editora.getId_editora() == id)
+                retorno = editora;
+        }
+        return retorno;
+    }
     public static boolean alterar(Editora _obj){
         return EditoraDal.alteraEditora(_obj);
     }

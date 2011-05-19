8dd0a32ce930fce8c90b52164dae45840a1d4bea
alexandrejaenicke@gmail.com <alexandrejaenicke@gmail.com@7d76fdf4-5015-b27e-b386-42a4ce47dd1f>
2011-05-19 16:10:58 +0000

    Classe de conversão de data

---
+++ trunk/AppWeb.Repository/src/una/pa/repository/ConvData.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package una.pa.repository;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ConvData{
+
+   public static String parseDataBra(String dataIso)
+   {
+      SimpleDateFormat formatIso = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+      SimpleDateFormat formatBra = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+      try
+      {
+         Date date = formatIso.parse(dataIso);
+         return(formatBra.format(date));
+      }
+      catch(ParseException e)
+      {
+         e.printStackTrace();
+         return("Parse Date Error");
+      }
+   }
+}

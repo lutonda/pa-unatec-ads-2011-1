cffcda5d861f24e4ac04055c6bc0bbb617b9eee7
tiagokiokushin <tiagokiokushin@gmail.com>
2011-05-21 18:27:17 +0000

    Criando listagem de Usuarios

--- AppWeb.Model/src/una/pa/model/Desejousuario.java
+++ AppWeb.Model/src/una/pa/model/Desejousuario.java
@@ -5,14 +5,18 @@
 
 package una.pa.model;
 
+import javax.xml.crypto.Data;
+
 /**
  *
  * @author Tiago
  */
 public class Desejousuario extends Jogo{
-    private String nm_titulo;
-    private String ds_console;
+    private int id_jogo_desejado;
+    private int id_usuario;
+    private Data dt_solicitacao;
     private int nivel_desejo;
+    private String ds_console;
 
     public String getDs_console() {
         return ds_console;
@@ -20,6 +24,30 @@ public class Desejousuario extends Jogo{
 
     public void setDs_console(String ds_console) {
         this.ds_console = ds_console;
+    }
+
+    public Data getDt_solicitacao() {
+        return dt_solicitacao;
+    }
+
+    public void setDt_solicitacao(Data dt_solicitacao) {
+        this.dt_solicitacao = dt_solicitacao;
+    }
+
+    public int getId_jogo_desejado() {
+        return id_jogo_desejado;
+    }
+
+    public void setId_jogo_desejado(int id_jogo_desejado) {
+        this.id_jogo_desejado = id_jogo_desejado;
+    }
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
     }
 
     public int getNivel_desejo() {
@@ -30,12 +58,5 @@ public class Desejousuario extends Jogo{
         this.nivel_desejo = nivel_desejo;
     }
 
-    public String getNm_titulo() {
-        return nm_titulo;
-    }
-
-    public void setNm_titulo(String nm_titulo) {
-        this.nm_titulo = nm_titulo;
-    }
-
+  
 }

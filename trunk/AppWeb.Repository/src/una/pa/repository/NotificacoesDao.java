0fc5f80ea68572e60ddc59cf22b8a9cec25184c3
tiagokiokushin@gmail.com <tiagokiokushin@gmail.com@7d76fdf4-5015-b27e-b386-42a4ce47dd1f>
2011-05-21 22:14:57 +0000

    Criando listagem de Usuarios

--- trunk/AppWeb.Repository/src/una/pa/repository/NotificacoesDao.java
+++ trunk/AppWeb.Repository/src/una/pa/repository/NotificacoesDao.java
@@ -9,7 +9,6 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import una.pa.model.*;
-import una.pa.repository.ConvData;
 import java.util.List;
 
 
@@ -56,5 +55,47 @@ public class NotificacoesDao {
         }catch(Exception e){
         return null;
     }
-}
+    }
+
+    public static List<Notificacoes> listarUnico(int _id){
+        List<Notificacoes> objct = new ArrayList<Notificacoes>();
+
+         String sql = "select "
+            + " notificacoes.id_notificacao, "
+            + " usuario.id_usuario, "
+            + " usuario.nm_usuario, "
+            + "	notificacoes.descricao, "
+            + "	notificacoes.broadcast, "
+            + "	notificacoes.dt_notificacao "
+            + " from  usuario "
+            + "	inner join amigo_usuario on usuario.id_usuario = amigo_usuario.id_usuario "
+            + "	inner join notificacoes	on amigo_usuario.id_amigo_usuario = notificacoes.id_amigo_usuario "
+            + " where usuario.id_usuario = ? ";
+
+         Object [] vetor = {_id};
+
+         try{
+             Connection c = Data.openConnection();
+             ResultSet rs = Data.executeQuery(c, sql, vetor);
+
+             while(rs.next()){
+               Notificacoes o = new Notificacoes();
+               o.setId_notificacao(Integer.parseInt(rs.getString("id_notificacao")));
+               o.setId_usuario(Integer.parseInt(rs.getString("id_usuario")));
+               o.setNm_nome(rs.getString("nm_usuario"));
+               o.setDescricao(rs.getString("descricao"));
+               o.setBroadcast(rs.getString("broadcast"));
+               String data = ConvData.parseDataBra(rs.getString("dt_notificacao"));
+               o.setDt_notificacoes(data);
+               objct.add(o);
+             }
+             rs.close();
+             c.close();
+             return objct;
+
+         }catch(Exception e ){
+             return null;
+         }
+
+    }
 }

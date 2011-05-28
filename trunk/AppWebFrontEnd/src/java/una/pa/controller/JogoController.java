a3ed6f8973f5a57cd4847c3dd5d1df2435cebe65
tiagokiokushin@gmail.com <tiagokiokushin@gmail.com@7d76fdf4-5015-b27e-b386-42a4ce47dd1f>
2011-05-28 01:01:44 +0000

    Jogo

--- trunk/AppWebFrontEnd/src/java/una/pa/controller/JogoController.java
+++ trunk/AppWebFrontEnd/src/java/una/pa/controller/JogoController.java
@@ -2,13 +2,14 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package una.pa.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+import una.pa.model.Jogo;
+import una.pa.service.JogoService;
 
 /**
  *
@@ -18,7 +19,17 @@ public class JogoController extends MultiActionController {
 
     public ModelAndView detalhesjogo(HttpServletRequest request,
             HttpServletResponse response) throws Exception {
+            ModelAndView mav = new ModelAndView("site/jogo/detalhesjogo");
+        try {
+            int id = Integer.parseInt(request.getParameter("id"));
 
-        return new ModelAndView("site/jogo/detalhesjogo", "msg", "teste");
+            Jogo objct = JogoService.detalheJogo(id);
+            mav.addObject("Jogo", objct);
+
+        } catch (Exception e) {
+            response.sendRedirect("/site/inicio/index.html");
+            return null;
+        }
+        return mav;
     }
 }

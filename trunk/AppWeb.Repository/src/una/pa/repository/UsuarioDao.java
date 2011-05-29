7cc19400af6a49e3f32fa79a3734e1acd30f2352
tiagokiokushin@gmail.com <tiagokiokushin@gmail.com@7d76fdf4-5015-b27e-b386-42a4ce47dd1f>
2011-05-29 23:46:00 +0000

    detalhes jogo

--- trunk/AppWeb.Repository/src/una/pa/repository/UsuarioDao.java
+++ trunk/AppWeb.Repository/src/una/pa/repository/UsuarioDao.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import sun.misc.Request;
 import una.pa.model.*;
 
 /**
@@ -144,14 +145,23 @@ public class UsuarioDao {
                     " FROM	JOGO_DESEJADO, USUARIO " +
                     " WHERE ID_JOGO = ? " +
                     "	AND JOGO_DESEJADO.ID_USUARIO = USUARIO.ID_USUARIO " +
+                    " UNION ALL " +
+                    " SELECT	USUARIO.ID_USUARIO, USUARIO.NM_USUARIO, " +
+                    "		USUARIO.NM_SOBRENOME, " +
+                    "   	'Oferta' STATUS, " +
+                    "		3 ID " +
+                    " FROM JOGO_USUARIO, USUARIO " +
+                    " WHERE JOGO_USUARIO.ID_USUARIO = USUARIO.ID_USUARIO" +
+                    " AND NIVEL_INTERESSE = 0 " +
+                    " AND ID_JOGO = ? " +
                     " )A " +
                     " WHERE ID  = case when ? = 0 then ID else ? end " ;
 
-        Object[] vetor = {_id, _id, _cod, _cod};
-        try {
+        Object[] vetor = {_id, _id,_id, _cod, _cod};
+        try{
             Connection c = Data.openConnection();
             ResultSet rs = Data.executeQuery(c, sql, vetor);
-            while (rs.next()) {
+            while(rs.next()){
                 Usuario o = new Usuario();
                 o.setId_usuario(Integer.parseInt(rs.getString("id_usuario")));
                 o.setNm_usuario(rs.getString("nm_usuario"));
@@ -161,61 +171,7 @@ public class UsuarioDao {
             }
             return objct;
 
-        } catch (Exception e) {
-            return null;
-        }
-    }
-
-    public static DadosIniciais inicioPerfil(String username) {
-
-        String sql = "select u.id_usuario "
-                + ", u.nm_usuario "
-                + ", u.nm_sobrenome "
-                + ", u.imagem "
-                + ", isnull((select avg(pontos) from avaliacao_usuario where id_usuario = u.id_usuario),0) as pontos "
-                + ", isnull((select count(*) from (select * from troca t inner join jogo_usuario juo on t.id_jogo_origem = juo.id_jogo_usuario union "
-                + "select * from troca t inner join jogo_usuario jud on t.id_jogo_destino = jud.id_jogo_usuario) tabela "
-                + "where id_usuario = u.id_usuario and status_troca = 'F'),0) as trocas "
-                + ", isnull((select count(*) from (select * from troca t inner join jogo_usuario juo on t.id_jogo_origem = juo.id_jogo_usuario union "
-                + "select * from troca t inner join jogo_usuario jud on t.id_jogo_destino = jud.id_jogo_usuario) tabela "
-                + "where id_usuario = u.id_usuario and status_troca = 'A'),0) as trocas_pendentes "
-                + ", isnull((select count(*) from (select * from troca t inner join jogo_usuario juo on t.id_jogo_origem = juo.id_jogo_usuario union "
-                + "select * from troca t inner join jogo_usuario jud on t.id_jogo_destino = jud.id_jogo_usuario) tabela "
-                + "where id_usuario = u.id_usuario and status_troca = 'P'),0) as propostas "
-                + ", isnull((select count(*) from jogo_usuario ju where ju.id_usuario = u.id_usuario and nivel_interesse = 0),0) as oferta "
-                + ", isnull((select count(*) from jogo_usuario ju where ju.id_usuario = u.id_usuario),0) as jogos "
-                + ", isnull((select count(*) from jogo_desejado jd where jd.id_usuario = u.id_usuario),0) as desejo "
-                + ", isnull((select count(*) from avaliacao_usuario au left join replica_avaliacao ra on au.id_avaliacao_usuario = ra.id_avaliacao_usuario where au.id_usuario = u.id_usuario and ra.id_replica is null),0) as replica_pendente "
-                + ", isnull((select count(*) from amigo_usuario aiu where aiu.id_usuario = u.id_usuario and aiu.sn_aceite = 0),0) as amigos_pendentes "
-                + " from usuario u "
-                + "where u.usuario = ?";
-
-        Object[] vetor = {username};
-
-        try {
-            Connection c = Data.openConnection();
-            ResultSet rs = Data.executeQuery(c, sql, vetor);
-            DadosIniciais o = new DadosIniciais();
-
-            if (rs.next()) {
-                o.setId_usuario(Integer.parseInt(rs.getString("id_usuario")));
-                o.setNm_usuario(rs.getString("nm_usuario"));
-                o.setNm_sobrenome(rs.getString("nm_sobrenome"));
-                o.setPontos(Integer.parseInt(rs.getString("pontos")));
-                o.setTrocas(Integer.parseInt(rs.getString("trocas")));
-                o.setTrocas_pendentes(Integer.parseInt(rs.getString("trocas_pendentes")));
-                o.setPropostas(Integer.parseInt(rs.getString("propostas")));
-                o.setOferta(Integer.parseInt(rs.getString("oferta")));
-                o.setJogos(Integer.parseInt(rs.getString("jogos")));
-                o.setDesejo(Integer.parseInt(rs.getString("desejo")));
-                o.setReplica_pendente(Integer.parseInt(rs.getString("replica_pendente")));
-                o.setAmigos_pendentes(Integer.parseInt(rs.getString("amigos_pendentes")));
-
-            }
-            rs.close();
-            c.close();
-            return o;
-        } catch (Exception e) {
+        }catch(Exception e){
             return null;
         }
     }

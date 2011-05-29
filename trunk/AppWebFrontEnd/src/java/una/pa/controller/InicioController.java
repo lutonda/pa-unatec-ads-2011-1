dbdd28423fdd845250eff6d9d348223198b99be1
ciqueira <ciqueira@7d76fdf4-5015-b27e-b386-42a4ce47dd1f>
2011-05-29 22:10:50 +0000

    inicial perfil

--- trunk/AppWebFrontEnd/src/java/una/pa/controller/InicioController.java
+++ trunk/AppWebFrontEnd/src/java/una/pa/controller/InicioController.java
@@ -4,12 +4,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+import una.pa.model.*;
+import una.pa.service.*;
+import java.util.*;
 
 public class InicioController extends MultiActionController {
 
     public ModelAndView index(HttpServletRequest request,
             HttpServletResponse response) throws Exception {
+        ModelAndView mav = new ModelAndView("site/inicio/index");
+        try {
 
-        return new ModelAndView("site/inicio/index", "msg", "teste");
+            DadosIniciais obj = UsuarioService.inicioPerfil("magno");
+            List<Tags> objTags = TagsService.listarTags(obj.getId_usuario());
+            mav.addObject("DadosIniciais", obj);
+            mav.addObject("tags", objTags);
+
+        } catch (Exception e) {
+            return null;
+        }
+        return mav;
     }
 }

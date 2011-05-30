4f965275e0119f3cfa8e5782ea50233ec375f84a
ciqueira <ciqueira@7d76fdf4-5015-b27e-b386-42a4ce47dd1f>
2011-05-30 05:38:11 +0000

    melhorias amigos

--- AppWeb.Service/src/una/pa/service/AmigoUsuarioService.java
+++ AppWeb.Service/src/una/pa/service/AmigoUsuarioService.java
@@ -5,7 +5,7 @@
 
 package una.pa.service;
 
-import java.util.List;
+import java.util.*;
 import una.pa.model.*;
 import una.pa.repository.*;
 /**
@@ -16,7 +16,30 @@ public class AmigoUsuarioService {
     public static List<AmigoUsuario> listarAmigo(int _id){
         return  AmigoUsuarioDao.listarAmigo(_id);
     }
-    public static List<AmigoUsuario> detalheAmigo(int _id){
-        return AmigoUsuarioDao.detalheAmigos(_id);
+    public static List<AmigoUsuario> listarAmigos(int _id){
+        List<AmigoUsuario> lObjec = AmigoUsuarioDao.listarTodosAmigos(_id, true);
+
+        List<AmigoUsuario> retorno = new ArrayList<AmigoUsuario>();
+        for (Iterator<AmigoUsuario> it = lObjec.iterator(); it.hasNext();) {
+            AmigoUsuario amigoUsuario = it.next();
+
+            if (amigoUsuario.getSn_aceite() == 1)
+                retorno.add(amigoUsuario);
+        }
+        return retorno;
+    }
+
+    public static List<AmigoUsuario> listarAmigosPendentes(int _id){
+
+        List<AmigoUsuario> lObjec = AmigoUsuarioDao.listarTodosAmigos(_id, false);
+
+        List<AmigoUsuario> retorno = new ArrayList<AmigoUsuario>();
+        for (Iterator<AmigoUsuario> it = lObjec.iterator(); it.hasNext();) {
+            AmigoUsuario amigoUsuario = it.next();
+
+            if (amigoUsuario.getSn_aceite() == 0)
+                retorno.add(amigoUsuario);
+        }
+        return retorno;
     }
 }

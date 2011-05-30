32af9fccee62c72495b1cd581b4f57e132099c62
tiagokiokushin@gmail.com <tiagokiokushin@gmail.com@7d76fdf4-5015-b27e-b386-42a4ce47dd1f>
2011-05-30 03:41:26 +0000

    detalhes amigos

--- trunk/AppWebFrontEnd/src/java/una/pa/controller/AmigoController.java
+++ trunk/AppWebFrontEnd/src/java/una/pa/controller/AmigoController.java
@@ -5,10 +5,13 @@
 
 package una.pa.controller;
 
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+import una.pa.model.AmigoUsuario;
+import una.pa.repository.AmigoUsuarioDao;
 
 /**
  *
@@ -19,8 +22,12 @@ public class AmigoController extends MultiActionController {
     public ModelAndView amigos(HttpServletRequest request,
             HttpServletResponse response) throws Exception {
         ModelAndView mav = new ModelAndView("site/amigo/amigos");
-
+        
         try{
+            int id = Integer.parseInt(request.getParameter("id"));
+
+            List<AmigoUsuario> obj = AmigoUsuarioDao.detalheAmigos(id);
+            mav.addObject("amigos", obj);
             return mav;
         }catch(Exception e){
             return null;

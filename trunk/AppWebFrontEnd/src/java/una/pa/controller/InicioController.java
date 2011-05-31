0af18ca9268b6a0def03b78361fb42bb7852b55a
tiagokiokushin@gmail.com <tiagokiokushin@gmail.com@7d76fdf4-5015-b27e-b386-42a4ce47dd1f>
2011-05-31 01:27:48 +0000

    Validacao email

--- trunk/AppWebFrontEnd/src/java/una/pa/controller/InicioController.java
+++ trunk/AppWebFrontEnd/src/java/una/pa/controller/InicioController.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 import una.pa.model.*;
 import una.pa.service.*;
 import java.util.*;
+import una.pa.util.StringEncryptor;
 import una.pa.util.Validacao;
 
 public class InicioController extends MultiActionController {
@@ -39,7 +40,11 @@ public class InicioController extends MultiActionController {
                if(email != null){
                   if(Validacao.validaEmail(email)){
                         if(UsuarioService.verificaEmail(email)){
-                            response.sendRedirect("cadastroPasso1.html?email=" + email);
+
+                            StringEncryptor ec = new StringEncryptor();
+                            String id = ec.encrypt("4");
+
+                            response.sendRedirect("cadastroPasso1.html?id=" + id);
                         }else{
                              response.sendRedirect("../ajuda/orientacao.html");
                         }
@@ -58,8 +63,48 @@ public class InicioController extends MultiActionController {
             HttpServletResponse response) throws Exception {
         ModelAndView mav = new ModelAndView("site/inicio/cadastroPasso1");
         try {
+            StringEncryptor ec = new StringEncryptor();
+            mav.addObject("msg", ec.decrypt(request.getParameter("id")));
 
 
+
+        } catch (Exception e) {
+            return null;
+        }
+        return mav;
+    }
+
+    public ModelAndView cadastroPasso2 (HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+        ModelAndView mav = new ModelAndView("site/inicio/cadastroPasso2");
+        try {
+
+
+
+        } catch (Exception e) {
+            return null;
+        }
+        return mav;
+    }
+
+    public ModelAndView cadastroPasso1Form (HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+        ModelAndView mav = new ModelAndView("site/inicio/cadastroPasso1");
+        try {
+
+response.sendRedirect("cadastroPasso2.html");
+
+        } catch (Exception e) {
+            return null;
+        }
+        return mav;
+    }
+    public ModelAndView cadastroPasso2Form (HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+        ModelAndView mav = new ModelAndView("site/inicio/cadastroPasso1");
+        try {
+
+response.sendRedirect("index.html");
 
         } catch (Exception e) {
             return null;

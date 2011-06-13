8448ad64e4633df9d0ea5b7da461612c47b2f53a
ciqueira <ciqueira@7d76fdf4-5015-b27e-b386-42a4ce47dd1f>
2011-06-13 03:11:39 +0000

    listagem usuarios em detalhes jogos

--- AppWeb.Service/src/una/pa/service/UsuarioService.java
+++ AppWeb.Service/src/una/pa/service/UsuarioService.java
@@ -2,41 +2,48 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package una.pa.service;
+
 import com.sun.org.apache.regexp.internal.RE;
 import una.pa.model.*;
 import una.pa.repository.*;
 import java.util.*;
-
 
 /**
  *
  * @author Tiago
  */
 public class UsuarioService {
+
     public static List<Usuario> listar() {
         return UsuarioDao.listarPrincipal();
     }
-    public static Usuario listarUnico (int _id){
+
+    public static Usuario listarUnico(int _id) {
         return UsuarioDao.listarTodos(_id);
     }
-    public static List<Usuario> usuarioJogos(int _id, int _cod){
-       return UsuarioDao.jogoUsuario(_id,_cod);
+
+    public static List<Usuario> usuarioJogos(int _id, boolean _cod, int quantidePorPagina, int pagina, int tipo) {
+        return UsuarioDao.jogoUsuario(_id, _cod, quantidePorPagina, pagina, tipo);
     }
-    public static DadosIniciais inicioPerfil(){
+
+    public static DadosIniciais inicioPerfil() {
         return UsuarioDao.inicioPerfil("magno");
     }
-    public static DadosIniciais inicioPerfil(int _id){
+
+    public static DadosIniciais inicioPerfil(int _id) {
         return UsuarioDao.inicioPerfil(_id);
     }
-    public static int verificaEmail(String _email){
+
+    public static int verificaEmail(String _email) {
         return UsuarioDao.validaEmail(_email);
     }
-    public static boolean updatePasso1 (Usuario _obj){
+
+    public static boolean updatePasso1(Usuario _obj) {
         return UsuarioDao.updatePasso1(_obj);
     }
-    public static boolean updatePasso2 (Usuario _objU, Endereco _objE){
+
+    public static boolean updatePasso2(Usuario _objU, Endereco _objE) {
         return UsuarioDao.updatePasso2(_objU, _objE);
     }
 }

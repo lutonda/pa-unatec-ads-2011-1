7aedf33a6ff66709939ed1c409c81063bf6f8d42
ciqueira <ciqueira@7d76fdf4-5015-b27e-b386-42a4ce47dd1f>
2011-06-16 23:25:14 +0000

    ajustes diversos

--- AppWeb.Service/src/una/pa/service/DesejoUsuarioService.java
+++ AppWeb.Service/src/una/pa/service/DesejoUsuarioService.java
@@ -2,8 +2,8 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package una.pa.service;
+
 import java.util.List;
 import una.pa.model.*;
 import una.pa.repository.*;
@@ -13,8 +13,12 @@ import una.pa.repository.*;
  * @author Tiago
  */
 public class DesejoUsuarioService {
-    public static List<Desejousuario> listarDesejo(int _id){
+
+    public static List<Desejousuario> listarDesejo(int _id) {
         return DesejoUsuarioDao.listarDesejo(_id);
     }
 
+    public static List<Jogo> listaJogosDesejados(int pId_usuario, int quantidePorPagina, int pagina) {
+        return DesejoUsuarioDao.listaJogosDesejados(pId_usuario, quantidePorPagina, pagina);
+    }
 }

d2daf3cfe7939a647940d0fdf14aed1c2a39c294
ciqueira <ciqueira@7d76fdf4-5015-b27e-b386-42a4ce47dd1f>
2011-10-31 14:03:58 +0000

    ajustes amigos

--- AppWeb.Repository/src/una/pa/repository/CtrlUsuarioDao.java
+++ AppWeb.Repository/src/una/pa/repository/CtrlUsuarioDao.java
@@ -3,34 +3,49 @@ package una.pa.repository;
 import java.sql.Connection;
 
 public class CtrlUsuarioDao {
-    public static  boolean aceita(int _id){
-        
-        try{
+
+    public static boolean aceita(int _id) {
+
+        try {
             Connection c = Data.openConnection();
             String sql = "update amigo_usuario "
-                        + "set dt_aceite = getDate(), "
-                        + "sn_aceite = 1 "
-                        + "where  id_amigo_usuario = ?";
+                    + "set dt_aceite = getDate(), "
+                    + "sn_aceite = 1 "
+                    + "where  id_amigo_usuario = ?";
 
             Object[] vetor = {_id};
             Data.executeUpdate(c, sql, vetor);
             c.close();
             return true;
-        }catch (Exception e) {
+        } catch (Exception e) {
             return false;
         }
     }
-    public static boolean recusa(int _id){
-        try{
+
+    public static boolean recusa(int _id) {
+        try {
             Connection c = Data.openConnection();
             String sql = "delete amigo_usuario where id_amigo_usuario = ? ";
             Object[] vetor = {_id};
             Data.executeUpdate(c, sql, vetor);
             c.close();
             return true;
-        }catch(Exception e){
+        } catch (Exception e) {
             return false;
         }
     }
 
+    public static boolean adicionar(int _id, int _idUserVis) {
+        try {
+            Connection c = Data.openConnection();
+            String sql = "insert into amigo_usuario (id_usuario,id_usuario_amigo,dt_convite,sn_aceite,ignorado)"
+                    + " values(?,?,getdate(),0,0) ";
+            Object[] vetor = {_idUserVis, _id};
+            Data.executeUpdate(c, sql, vetor);
+            c.close();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }

09361dc06f64831f45e9e433527b9c90cc0d0989
tiagokiokushin@gmail.com <tiagokiokushin@gmail.com@7d76fdf4-5015-b27e-b386-42a4ce47dd1f>
2011-11-02 17:33:00 +0000

    Filtros com paginacao, atualizacao function retorna total registros

--- trunk/AppWeb.Service/src/una/pa/service/JogoService.java
+++ trunk/AppWeb.Service/src/una/pa/service/JogoService.java
@@ -71,4 +71,7 @@ public class JogoService {
     public static List<Jogo> filtroMaisTrocados(int quantidePorPagina, int pagina ){
         return JogoDal.FiltroJogosTrocados(quantidePorPagina,pagina);
     }
+    public static List<Jogo> filtroTop10(int quantidePorPagina, int pagina ){
+        return JogoDal.FiltroTop10(quantidePorPagina,pagina);
+    }
 }

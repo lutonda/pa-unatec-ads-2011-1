8d683575505dfedb2da4b91e717047e725b933c8
ciqueira <ciqueira@7d76fdf4-5015-b27e-b386-42a4ce47dd1f>
2011-11-06 18:28:46 +0000

    ajustes perfil

--- trunk/AppWeb.Service/src/una/pa/service/CtrlUsuarioService.java
+++ trunk/AppWeb.Service/src/una/pa/service/CtrlUsuarioService.java
@@ -6,6 +6,9 @@ public class CtrlUsuarioService {
     public static boolean aceitaUsuario(int _id){
         return CtrlUsuarioDao.aceita(_id);
     }
+    public static boolean ignorarUsuario(int _id, int _ign){
+        return CtrlUsuarioDao.ignorar(_id, _ign);
+    }
     public static boolean  recusaUsuario(int _id){
         return CtrlUsuarioDao.recusa(_id);
     }

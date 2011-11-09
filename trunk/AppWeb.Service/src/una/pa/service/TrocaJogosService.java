d8ed47bfb07d8580999b36311dcb5cfe6bfca5d2
tiagokiokushin@gmail.com <tiagokiokushin@gmail.com@7d76fdf4-5015-b27e-b386-42a4ce47dd1f>
2011-11-09 01:32:55 +0000

    Troca de Jogos

--- trunk/AppWeb.Service/src/una/pa/service/TrocaJogosService.java
+++ trunk/AppWeb.Service/src/una/pa/service/TrocaJogosService.java
@@ -8,11 +8,14 @@ import java.util.*;
  * @author Tiago
  */
 public class TrocaJogosService {
-       public static List<TrocaJogos> listar() {
+    public static List<TrocaJogos> listar() {
            return TrocaJogosDao.listarDao();
     }
-       public static List<TrocaJogos> listarUnico(int _id){
+    public static List<TrocaJogos> listarUnico(int _id){
            return TrocaJogosDao.listarUnico(_id);
        }
+    public static List<TrocaJogos> jogosTrocados(int quantidePorPagina, int pagina, int _id, String _retorno)   {
+        return TrocaJogosDao.jogosTrocados(quantidePorPagina, pagina, _id, _retorno);
+    }
 
 }

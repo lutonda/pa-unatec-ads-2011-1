4ce19f03a5a7794f82a27ad05d3ac39a2beaa6d4
tiagokiokushin@gmail.com <tiagokiokushin@gmail.com@7d76fdf4-5015-b27e-b386-42a4ce47dd1f>
2011-11-10 01:50:23 +0000

    Troca de Jogos

--- trunk/AppWebFrontEnd/web/resources/js/validacaoPasso2.js
+++ trunk/AppWebFrontEnd/web/resources/js/validacaoPasso2.js
@@ -78,4 +78,11 @@ $('#formCadastro2').validate({
                 required: "Informe um Estado"
             }
         }
+    }),
+
+    jQuery(function($){
+       $("#date").mask("99/99/9999");
+       //$("#phone").mask("(999) 999-9999");
+       //$("#tin").mask("99-9999999");
+       //$("#ssn").mask("999-99-9999");
     });

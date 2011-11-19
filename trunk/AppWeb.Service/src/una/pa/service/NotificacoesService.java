896e300b9cde086fc9d5fd4c3ccf8d7e701fa4ec
tiagokiokushin@gmail.com <tiagokiokushin@gmail.com@7d76fdf4-5015-b27e-b386-42a4ce47dd1f>
2011-11-19 19:22:28 +0000

    Troca de jogos

--- trunk/AppWeb.Service/src/una/pa/service/NotificacoesService.java
+++ trunk/AppWeb.Service/src/una/pa/service/NotificacoesService.java
@@ -7,6 +7,8 @@ package una.pa.service;
 import una.pa.model.*;
 import una.pa.repository.*;
 import java.util.List;
+import una.pa.util.*;
+import javax.rmi.CORBA.Util;
 
 /**
  *
@@ -16,6 +18,16 @@ public class NotificacoesService {
     public static List<Notificacoes> listar (){
         return NotificacoesDao.listarDao();
         //
+    }
+    public static enum numeraNotificacao{
+        TROCA,
+        FALA,
+        ADICIONA,
+        ENTRA,
+        DESEJA,
+        TEM,
+        OFERTA,
+        AVALIADO
     }
 
     public static List<Notificacoes> listarUnico(int _id){
@@ -28,15 +40,50 @@ public class NotificacoesService {
         return NotificacoesDao.enviaNotificacao(objct);
     }
     
-    public static boolean enviaNotificacao(int _idUser, int _idUserDestino, String _nomeUserDestino, int idJogoOrg, String _nmJogoOrg, int idJogoDst, String _nmJogoDst){
+    public static boolean enviaNotificacao(numeraNotificacao hh,
+           String _msg,
+           int _idUser,
+           int _idUserDestino,
+           String _nomeUserDestino,
+           int idJogoOrg,
+           String _nmJogoOrg,
+           int idJogoDst,
+           String _nmJogoDst,
+           int _pts){
         
-        String descricao = "trocou o jogo " + _nmJogoOrg + " por " + _nmJogoDst + " com " + _nomeUserDestino ;
-        
+        String descricao = "" ;
+
+        switch(hh){
+            case TROCA:
+                descricao = " trocou o jogo " + _nmJogoOrg + " por " + _nmJogoDst + " com " + _nomeUserDestino;
+                break;
+            case FALA:
+                descricao = " fala com " + _nomeUserDestino+ " "+ _msg ;
+                break;
+            case ADICIONA:
+                descricao =  " adicionou " +_nomeUserDestino +" ao amigos " ;
+                break;
+            case ENTRA:
+                descricao = " Entrou no Jogatroca " ;
+                break;
+            case DESEJA:
+                descricao = " Desejou ter o jogo "+ _nmJogoDst ;
+                break;
+            case TEM:
+                descricao = "  Tem o jogo " + _nmJogoDst;
+                break;
+            case OFERTA:
+                descricao = " Ofertou o jogo ";
+                break;
+            case AVALIADO:
+                descricao = "Foi avaliado com " + _pts + " pts por " + _nomeUserDestino;
+                break;
+        }
+
         Notificacoes objNot = new Notificacoes();
         objNot.setId_usuario(_idUser);
         objNot.setBroadcast((false)? 1:0);
         objNot.setDescricao(descricao);
-
         return NotificacoesDao.enviaNotificacao(objNot);
     }
 }

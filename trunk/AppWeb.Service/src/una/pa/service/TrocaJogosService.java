0ce78017eac9e69378b90818a5ac00ecf70a8e93
ciqueira <ciqueira@7d76fdf4-5015-b27e-b386-42a4ce47dd1f>
2011-11-21 19:38:35 +0000

    lista troca de jogos

--- trunk/AppWeb.Service/src/una/pa/service/TrocaJogosService.java
+++ trunk/AppWeb.Service/src/una/pa/service/TrocaJogosService.java
@@ -1,4 +1,5 @@
 package una.pa.service;
+
 import una.pa.model.*;
 import una.pa.repository.*;
 import java.util.*;
@@ -8,23 +9,28 @@ import java.util.*;
  * @author Tiago
  */
 public class TrocaJogosService {
+
     public static List<TrocaJogos> listar() {
-           return TrocaJogosDao.listarDao();
-    }
-    public static List<TrocaJogos> listarUnico(int _id){
-           return TrocaJogosDao.listarUnico(_id);
-       }
-    public static List<TrocaJogos> jogosTrocados(int quantidePorPagina, int pagina, int _id, String _retorno)   {
-        return TrocaJogosDao.jogosTrocados(quantidePorPagina, pagina, _id, _retorno);
-    }
-    public static boolean inserir(TrocaJogos _obj){
-        return TrocaJogosDao.incluir(_obj);
-    }
-    public static boolean excluir(int _id){
-        return TrocaJogosDao.excluir(_id);
-    }
-    public static boolean editar(TrocaJogos _obj){
-        return TrocaJogosDao.alterar(_obj);
+        return TrocaJogosDao.listarDao();
     }
 
+    public static List<TrocaJogos> listarUnico(int _id) {
+        return TrocaJogosDao.listarUnico(_id);
+    }
+
+    public static List<TrocaJogos> jogosTrocados(int quantidePorPagina, int pagina, int _id) {
+        return TrocaJogosDao.jogosTrocados(quantidePorPagina, pagina, _id);
+    }
+
+    public static boolean inserir(TrocaJogos _obj) {
+        return TrocaJogosDao.incluir(_obj);
+    }
+
+    public static boolean excluir(int _id) {
+        return TrocaJogosDao.excluir(_id);
+    }
+
+    public static boolean editar(TrocaJogos _obj) {
+        return TrocaJogosDao.alterar(_obj);
+    }
 }

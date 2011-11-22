5904d0374d6f5bd5b0ec335379bdf567eef73ce3
tiagokiokushin <tiagokiokushin@gmail.com>
2011-11-22 14:42:25 +0000

    Desejo Usuario

--- AppWeb.Service/src/una/pa/service/DesejoUsuarioService.java
+++ AppWeb.Service/src/una/pa/service/DesejoUsuarioService.java
@@ -21,4 +21,13 @@ public class DesejoUsuarioService {
     public static List<Jogo> listaJogosDesejados(int pId_usuario, int quantidePorPagina, int pagina) {
         return DesejoUsuarioDao.listaJogosDesejados(pId_usuario, quantidePorPagina, pagina);
     }
+    public static boolean  incluir(Desejousuario _obj){
+        return DesejoUsuarioDao.incluir(_obj);
+    }
+    public static boolean alterar(Desejousuario _obj){
+        return DesejoUsuarioDao.alterar(_obj);
+    }
+    public static boolean excluir(int _id){
+        return DesejoUsuarioDao.excluir(_id);
+    }
 }

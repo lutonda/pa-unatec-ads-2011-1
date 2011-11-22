efa4a29d1fbaa2e11ffdd768889dcd0b26c54fab
tiagokiokushin <tiagokiokushin@gmail.com>
2011-11-22 14:56:15 +0000

    Jogo usuario

--- AppWeb.Repository/src/una/pa/repository/JogoUsuarioDao.java
+++ AppWeb.Repository/src/una/pa/repository/JogoUsuarioDao.java
@@ -53,5 +53,47 @@ public class JogoUsuarioDao {
             return null;
         }
     }
+    public static boolean incluir(Jogousuario _obj){
+        try{
+            Connection c = Data.openConnection();
+            String sql = "insert into dbo.JOGO_USUARIO(id_usuario,id_jogo,est_capa,est_midia,est_manual,dt_cadastro,regiao_jogo,descricao,nivel_interesse)"
+                        +"values(?,?,?,?,?,getdate(),?,?,?)";
+            Object[] vetor = {_obj.getId_usuario(),_obj.getId_jogo(),_obj.getEst_capa(),_obj.getEst_midia(),_obj.getEst_manual(),_obj.getRegiao_jogo(),_obj.getDescricao(),_obj.getNivelInteresse()};
+            Data.executeUpdate(c, sql, vetor);
+            c.close();
+            return true;
+
+        }catch(Exception e){
+            return false;
+        }
+    }
+    public static boolean alterar(Jogousuario _obj){
+        try{
+            Connection c = Data.openConnection();
+            String sql = "update dbo.JOGO_USUARIO set id_usuario = ?,id_jogo = ?,est_capa = ?,est_midia = ?,est_manual = ?,dt_cadastro = getdate(),regiao_jogo = ?,descricao = ?,nivel_interesse = ? where id_jogo_usuario = ?";
+            Object[] vetor = {_obj.getId_usuario(),_obj.getId_jogo(),_obj.getEst_capa(),_obj.getEst_midia(),_obj.getEst_manual(),_obj.getRegiao_jogo(),_obj.getDescricao(),_obj.getNivelInteresse()};
+
+            Data.executeQuery(c, sql, vetor);
+            c.close();
+            return true;
+        }catch(Exception e){
+            return false;
+        }
+    }
+     public static boolean excluir(int _id) {
+
+        try {
+            Connection c = Data.openConnection();
+            String sql = "delete dbo.JOGO_USUARIO where where id_jogo_usuario = ?";
+            Object[] vetor = {_id};
+
+            Data.executeUpdate(c, sql, vetor);
+            c.close();
+            return true;
+
+        } catch (Exception e) {
+            return false;
+        }
+    }
 
 }

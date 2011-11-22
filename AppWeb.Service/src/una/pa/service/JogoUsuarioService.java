562765c45efb4387b7bf196abdd4b5c7beca49c1
tiagokiokushin <tiagokiokushin@gmail.com>
2011-11-22 14:56:34 +0000

    Jogo usuario

--- AppWeb.Service/src/una/pa/service/JogoUsuarioService.java
+++ AppWeb.Service/src/una/pa/service/JogoUsuarioService.java
@@ -16,5 +16,14 @@ public class JogoUsuarioService {
     public static List<Jogousuario> listarJogoUsuario(int _id){
         return JogoUsuarioDao.listarJogoUsuario(_id);
     }
+    public static boolean incluir(Jogousuario _obj){
+        return JogoUsuarioDao.incluir(_obj);
+    }
+    public static boolean alterar(Jogousuario _obj){
+        return JogoUsuarioDao.alterar(_obj);
+    }
+    public static boolean excluir(int _id){
+        return JogoUsuarioDao.excluir(_id);
+    }
 
 }

fc7372a00aee90a12ee331da2d021f3e0cad3ac3
tiagokiokushin@gmail.com <tiagokiokushin@gmail.com@7d76fdf4-5015-b27e-b386-42a4ce47dd1f>
2011-12-06 12:19:22 +0000

    Notificacoes

--- trunk/AppWeb.Repository/src/una/pa/repository/NotificacoesDao.java
+++ trunk/AppWeb.Repository/src/una/pa/repository/NotificacoesDao.java
@@ -59,17 +59,16 @@ public class NotificacoesDao {
     public static List<Notificacoes> listarUnico(int _id) {
         List<Notificacoes> objct = new ArrayList<Notificacoes>();
 
-        String sql = "select "
-                + " notificacoes.id_notificacao, "
-                + " usuario.id_usuario, "
-                + " usuario.nm_usuario, "
-                + " notificacoes.descricao, "
-                + " notificacoes.broadcast, "
-                + " notificacoes.dt_notificacao "
-                + " from  usuario "
-               // + "	inner join amigo_usuario on usuario.id_usuario = amigo_usuario.id_usuario "
-                + "	inner join notificacoes	on usuario.id_usuario = notificacoes.id_usuario "
-                + " where usuario.id_usuario = ? ";
+        String sql =  "select "
+                + "      notificacoes.id_notificacao, "
+                + "      usuario.id_usuario, "
+                + "      usuario.nm_usuario, "
+                + "      notificacoes.descricao, "
+                + "      notificacoes.broadcast, "
+                + "      notificacoes.dt_notificacao"
+                + "      from  usuario "
+                + "     inner join notificacoes	on usuario.id_usuario = notificacoes.id_usuario "
+                + "      where usuario.id_usuario = ? ";
 
         Object[] vetor = {_id};
 
@@ -85,6 +84,57 @@ public class NotificacoesDao {
                 o.setDescricao(rs.getString("descricao"));
                 o.setBroadcast(Integer.parseInt(rs.getString("broadcast")));
                 String data = ConvData.parseDataBra(rs.getString("dt_notificacao"));
+                o.setDt_notificacoes(data);
+                objct.add(o);
+            }
+            rs.close();
+            c.close();
+            return objct;
+
+        } catch (Exception e) {
+            return null;
+        }
+
+    }
+    public static List<Notificacoes> listarUnico(int _id, int quantidePorPagina, int pagina) {
+        List<Notificacoes> objct = new ArrayList<Notificacoes>();
+        int inicio = 0;
+        int fim = quantidePorPagina;
+
+        if (pagina > 1) {
+            fim = (quantidePorPagina * pagina);
+            inicio = fim - quantidePorPagina;
+        }
+
+        String sql = "select top " + quantidePorPagina + " * from ("
+                + "select row_number() over (order by dt_notificacao desc) as linha,"
+                + "      notificacoes.id_notificacao, "
+                + "      usuario.id_usuario, "
+                + "      usuario.nm_usuario, "
+                + "      notificacoes.descricao, "
+                + "      notificacoes.broadcast, "
+                + "      notificacoes.dt_notificacao,"
+                + "	  (select count(*) from notificacoes where id_usuario = ?)total_registros "
+                + "      from  usuario "
+                + "     	inner join notificacoes	on usuario.id_usuario = notificacoes.id_usuario "
+                + "      where usuario.id_usuario = ? )AS TABELA"
+                + "where linha > " + inicio+ " and linha <= " + fim ;
+
+        Object[] vetor = {_id,_id};
+
+        try {
+            Connection c = Data.openConnection();
+            ResultSet rs = Data.executeQuery(c, sql, vetor);
+
+            while (rs.next()) {
+                Notificacoes o = new Notificacoes();
+                o.setId_notificacao(Integer.parseInt(rs.getString("id_notificacao")));
+                o.setId_usuario(Integer.parseInt(rs.getString("id_usuario")));
+                o.setNm_nome(rs.getString("nm_usuario"));
+                o.setDescricao(rs.getString("descricao"));
+                o.setBroadcast(Integer.parseInt(rs.getString("broadcast")));
+                String data = ConvData.parseDataBra(rs.getString("dt_notificacao"));
+                o.setTotal(Integer.parseInt(rs.getString("total_registros")));
                 o.setDt_notificacoes(data);
                 objct.add(o);
             }

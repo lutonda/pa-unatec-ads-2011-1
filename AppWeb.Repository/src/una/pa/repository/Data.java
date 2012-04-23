ead4e0fa23c30beb5ff1223ebd716eeb604dcdb1
ciqueira <ciqueira@7d76fdf4-5015-b27e-b386-42a4ce47dd1f>
2012-04-23 20:59:35 +0000

    ajustes diversos

--- AppWeb.Repository/src/una/pa/repository/Data.java
+++ AppWeb.Repository/src/una/pa/repository/Data.java
@@ -18,7 +18,7 @@ import java.sql.Statement;
 public class Data {
 
     public static Connection openConnection() throws Exception {
-         return openConnectionSqlServer("NEWCOM-DESK550", "trocajogos", "sa", "");
+         return openConnectionSqlServer("localhost", "trocajogos", "sa", "sa");
     }
 
     public static Connection openConnectionPostgre(String server,
